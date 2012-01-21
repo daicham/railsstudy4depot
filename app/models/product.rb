c97d8da1c9330f7ffa4e97154db016a5ca5b904f
daicham <daicham@gmail.com>
2012-01-21 23:21:45 +0900

    P.77 title がユニークである validate を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
