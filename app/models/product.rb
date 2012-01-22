2333f9fb35e7b38133462bab5ec009a37a61e81d
daicham <daicham@gmail.com>
2012-01-22 16:44:58 +0900

    P.82 priceの拡張子のテストを追加
    プロダクションコードが間違っていたのも修正した。

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)}i,
+    with: %r{\.(gif|jpg|png)$}i,
     message: 'はGIF、JPG、PNG画像のURLでなければなりません'
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -27,4 +27,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price: 1,
+                image_url:   image_url)
+  end
+
+  test "image_url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.gif FRED.Jpg
+             http://a.b.c./x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
 end
