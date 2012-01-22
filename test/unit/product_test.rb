be8885ef71576417ce6091d85ab6b84f87abe2c5
daicham <daicham@gmail.com>
2012-01-22 16:59:34 +0900

    P.86 assert_equal しているエラーメッセージのハードコードをi18nから取るように変更

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -57,6 +57,7 @@ class ProductTest < ActiveSupport::TestCase
                           price: 1,
                           image_url: "fred.gif")
     assert !product.save
-    assert_equal "has already been taken", product.errors[:title].join(', ')
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+                 product.errors[:title].join('; ')
   end
 end
