ae0792a9ccbe9a5b875f7ccac4877b9bfb08be02
daicham <daicham@gmail.com>
2012-01-28 17:54:41 +0900

    P.103 Product に line_item を has_many で関連付ける
    before_destroy はこのオブジェクトを賛助する前に呼び出されるフックメソッドでrailsから自動的に呼び出される。
    before_destroy で品目から参照されていたら削除されないようにしている。
    DBから取るときに関連するline_itemもたぶん一緒にとってくるから、全ユーザのカートに入っているかどうかはこれで判断できると思われる
    
    errorsにメッセージを格納している。validate()のメッセージと同じ場所を使っている。ここでは個別属性と紐付けてなくベースオブジェクト自体とひも付けている

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :image_url, allow_blank: true, format: {
@@ -7,4 +10,15 @@ class Product < ActiveRecord::Base
     message: 'はGIF、JPG、PNG画像のURLでなければなりません'
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  private
+  # この商品を参照している品目がないことを確認する
+  def ensure_not_refernced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, '品目が存在します')
+      return false
+    end
+  end
 end
