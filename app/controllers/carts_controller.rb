fc17eb527504f3ee3accc165613986326b2c4e33
daicham <daicham@gmail.com>
2012-02-01 22:47:37 +0900

    P.139 カートがからの時の非表示の仕方を変更。
    * _cart.html.erbで空だったら表示しない処理は元に戻す
    * application_helper.rb に非表示divタグを生成するヘルパを作成
    * application.html.erb でそのヘルパを呼び出し、空の場合はカートを非表示にする
    * １つめのItemをカートにいれたときにカートの表示をアニメーションするようにする

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -83,7 +83,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'カートは現在空です' }
+      format.html { redirect_to store_url }
       format.json { head :ok }
     end
   end
