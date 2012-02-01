a07e5410b6d9496321030ba8fb79f66d9d3038b8
daicham <daicham@gmail.com>
2012-02-01 16:49:59 +0900

    P.114 rake db:rollback 実行。ロールバック成功

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120201071857) do
+ActiveRecord::Schema.define(:version => 20120201065426) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
