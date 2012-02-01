fe5a58e3ccc145acdacabb50a8b76e414ae78681
daicham <daicham@gmail.com>
2012-02-01 17:23:23 +0900

    P.119 カートページに空にするボタンを追加
    テストコードも追加。delete()の第3引数はセッションに対応するHash

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -78,11 +78,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'カートは現在空です' }
       format.json { head :ok }
     end
   end
