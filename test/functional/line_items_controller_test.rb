d038b0a26f7b34196a738025209780934eab7814
daicham <daicham@gmail.com>
2012-02-02 20:56:13 +0900

    P.143 Ajaxで品目を追加するテストケースを追加
    * xhr :post でXMLHttpRequest のPostを意味する
    * create メソッドに yml のruby.id を引数として渡す
    * assert_select でレスポンスの中身をテストする

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -45,4 +45,15 @@ class LineItemsControllerTest < ActionController::TestCase
 
     assert_redirected_to line_items_path
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
 end
